8d6e71a1178132bda45677f6f3f471428a2e97c2
Stephen Elliott <steell1890@gmail.com>
2014-03-26 00:13:21 -0400

    more refactoring

--- ReactiveSandbox/ReactiveSandbox/App.fs
+++ ReactiveSandbox/ReactiveSandbox/App.fs
@@ -21,27 +21,12 @@ let initialize_menu (window : MainWindow) =
         window.NewNodeMenuItem.Click
         |> Observable.map (fun _ -> CreateNode)
 
-    // produces a world updater for when the mouse moves
-    let move_update =
-        let move_handler (args : Input.MouseEventArgs) =
-            // new position relative to the canvas
-            let new_pos = args.GetPosition window.NodeCanvas
-            // whether or not the mouse is being pressed
-            let dragging = 
-                match args.LeftButton with
-                | Input.MouseButtonState.Pressed -> true
-                | _ -> false
-            // update the position of the old world, 
-            // and if the mouse button was released update the offset
-            fun { drag_offset=offset } -> { position=new_pos; drag_offset=if dragging then offset else None }
-        window.NodeCanvas.PreviewMouseMove |> Observable.map move_handler
-
     let add_node = window.NodeCanvas.Children.Add >> ignore
 
     Observable.merge new_pos_updater creation_updater
     |> Observable.pairwise
     |> Observable.choose    (function SetPosition(p), CreateNode -> Some(p) | _ -> None)
-    |> Observable.subscribe (new_node window move_update >> add_node)
+    |> Observable.subscribe (new_node window >> add_node)
 
 let loadWindow() =
     let window = MainWindow()
--- ReactiveSandbox/ReactiveSandbox/Node.fs
+++ ReactiveSandbox/ReactiveSandbox/Node.fs
@@ -3,31 +3,38 @@
 open System
 open System.Windows
 open System.Windows.Controls
+open System.Windows.Input
 
 open FSharp.Control
 open FSharp.Control.Observable
 
 open XamlTypes
 
-// offset:   Some(Point) = location on the rectangle the mouse is dragging from, or None if no drag is taking place
-// position: Point       = current position of the mouse
-type DragCommandData = { drag_offset : Point option; position : Point }
-let world0 = { drag_offset=None; position=new Point(0., 0.) }
+let initialize_rect_drag (node : NodeUI) (window : MainWindow) =
+    // produces a world updater for when the mouse moves
+    let move_update =
+        window.NodeCanvas.MouseMove
+        |> Observable.choose (
+            fun args -> 
+                if args.LeftButton = MouseButtonState.Pressed 
+                then Some(args.GetPosition window.Root) 
+                else None)
 
-let initialize_rect_drag (node : NodeUI) move_update =
     // produces a world updater for drag flag when mouse is pressed
-    let mouse_down_update = 
-        let mouse_down_handler (args : Input.MouseButtonEventArgs) =
-            let new_offset = args.GetPosition node.NodeRect
-            // update the world's offset
-            fun cd -> { cd with drag_offset=Some(new_offset); }
-        node.NodeRect.PreviewMouseDown |> Observable.map mouse_down_handler
+    let start_stop_update =
+        let start_update = 
+            node.NodeRect.MouseDown
+            |> Observable.filter (fun args -> args.ChangedButton = Input.MouseButton.Left)
+            |> Observable.map    (fun args -> Some(args.GetPosition node.Root))
+        let stop_update =
+            Observable.merge node.NodeRect.MouseUp window.NodeCanvas.MouseUp
+            |> Observable.filter (fun args -> args.ChangedButton = Input.MouseButton.Left)
+            |> Observable.map    (fun _ -> None)
+        Observable.merge start_update stop_update
 
     // one event that is fired if any of the events are fired
-    Observable.merge move_update mouse_down_update
-    |> Observable.scanAccumulate world0 // take the result of the fired event and update the world
-    |> Observable.filter (fun world -> world.drag_offset.IsSome) // ignore all updates where we're not dragging
-    |> Observable.map (fun world -> world.position - world.drag_offset.Value) // calculate the new position of the rectangle
+    Observable.combineLatest start_stop_update move_update
+    |> Observable.choose    (function Some(offset), position -> Some(position - offset) | _ -> None)
     |> Observable.subscribe (
         // update rectangle position
         fun new_position -> 
@@ -69,14 +76,14 @@ let initialize_deletion (node : NodeUI) (window : MainWindow) =
     node.DeleteMenu.Click 
     |> Observable.subscribe (fun _ -> window.NodeCanvas.Children.Remove node.Root |> ignore)
 
-let new_node (window : MainWindow) move_update (position : Point) : UIElement =
+let new_node (window : MainWindow) (position : Point) : UIElement =
     let node = NodeUI()
 
-    let drag_handler = initialize_rect_drag node move_update
+    let drag_handler = initialize_rect_drag node window
     let color_handler = initialize_color_change node
     let delete_handler = initialize_deletion node window
 
-    Canvas.SetLeft(node.Root, position.X)
-    Canvas.SetTop(node.Root, position.Y)
+    Canvas.SetLeft(node.Root, position.X)// - node.NodeCanvas.Width / 2.)
+    Canvas.SetTop(node.Root, position.Y)// - node.NodeCanvas.Height / 2.)
 
     node.Root :> UIElement
