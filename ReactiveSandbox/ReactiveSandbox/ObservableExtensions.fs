4f80c6fe010aaeefac54653db3e8605fb416b4f1
Stephen Elliott <steell1890@gmail.com>
2014-03-27 14:47:47 -0400

    moved RxAdapter methods

--- ReactiveSandbox/ReactiveSandbox/ObservableExtensions.fs
+++ ReactiveSandbox/ReactiveSandbox/ObservableExtensions.fs
@@ -4,3 +4,10 @@ open System
 
 let scanAccumulate (state : 'a) (obs : IObservable<'a -> 'a>) =
     Observable.scan (fun state' f -> f state') state obs
+
+let interval (timespan : TimeSpan) obs =
+    let timer = new System.Timers.Timer(timespan.TotalMilliseconds)
+    timer.Elapsed |> Observable.scan (fun state _ -> state + 1) -1
+
+let timestamp obs = 
+    Observable.map (fun x -> x, DateTime.Now) obs
