0d237bb4023d0f3331a1918da75ced05b3fc4686
Stephen Elliott <steell1890@gmail.com>
2014-04-03 15:34:07 -0400

    moved dragging logic out of node viewmodel

--- ReactiveSandbox/ReactiveSandbox/Node.fs
+++ ReactiveSandbox/ReactiveSandbox/Node.fs
@@ -11,69 +11,6 @@ open FSharp.Control.Observable
 open XamlTypes
 open UndoRecorder
 
-type DragCommand = Start | Stop
-
-let initialize_rect_drag (node : NodeUI) (window : MainWindow) (undo_record : UndoRecorder) =
-    // produces a world updater for when the mouse moves
-    let move_update =
-        window.NodeCanvas.MouseMove
-        |> Observable.choose (
-            fun args -> 
-                if args.LeftButton = MouseButtonState.Pressed 
-                then Some(args.GetPosition window.Root) 
-                else None)
-
-    let start_update = 
-        node.NodeRect.MouseDown
-        |> Observable.filter (fun args -> args.ChangedButton = MouseButton.Left)
-        |> Observable.map    (fun args -> Some(args.GetPosition node.Root))
-    let stop_update =
-        Observable.merge node.NodeRect.MouseUp window.NodeCanvas.MouseUp
-        |> Observable.filter (fun args -> args.ChangedButton = MouseButton.Left)
-        |> Observable.map    (fun _ -> None)
-
-    // produces a world updater for drag flag when mouse is pressed
-    let start_stop_update = Observable.merge start_update stop_update
-
-    let update_pos (position : Point) =
-        Canvas.SetLeft(node.Root, position.X)
-        Canvas.SetTop(node.Root, position.Y)
-
-    let move_node_action position () = update_pos position
-
-    //let move_node_command origin position = { redo=position; undo=origin }
-
-    let get_current_position() =
-        new Point(Canvas.GetLeft node.Root, Canvas.GetTop node.Root)
-
-    let drag_command_stream = 
-        node.NodeRect.MouseDown
-        |> Observable.filter (fun args -> args.ChangedButton = MouseButton.Left)
-        |> Observable.map    (fun args -> Start)
-        |> Observable.merge (
-            Observable.merge node.NodeRect.MouseUp window.NodeCanvas.MouseUp
-            |> Observable.filter (fun args -> args.ChangedButton = MouseButton.Left)
-            |> Observable.map    (fun _ -> Stop))
-        |> Observable.pairwise
-        |> Observable.choose (
-            function 
-                | Start, Stop -> Some(get_current_position())
-                | _ -> None)
-    
-    drag_command_stream
-    |> undo_record.RecordStreamState "Moved node" (get_current_position())
-    |> Observable.subscribe update_pos
-    |> ignore
-
-    // one event that is fired if any of the events are fired
-    Observable.combineLatest start_stop_update move_update
-    |> Observable.choose    (function Some(offset), position -> Some(position - offset) | _ -> None)
-    |> Observable.subscribe (
-        // update rectangle position
-        fun new_position -> 
-            Canvas.SetLeft(node.Root, new_position.X)
-            Canvas.SetTop(node.Root, new_position.Y))
-
 let initialize_color_change (node : NodeUI) (undo_recorder : UndoRecorder) =
     let node_click = 
         node.NodeButton.Click
@@ -113,14 +50,10 @@ let initialize_deletion (node : NodeUI) (window : MainWindow) (undo_record : Und
         | Redo(node) -> window.NodeCanvas.Children.Remove node
         | Undo(node) -> window.NodeCanvas.Children.Add node |> ignore)
 
-let new_node (window : MainWindow) (undo_record : UndoRecorder) (position : Point) : UIElement =
+let new_node (window : MainWindow) (undo_record : UndoRecorder) : NodeUI =
     let node = NodeUI()
 
-    Canvas.SetLeft(node.Root, position.X) // - node.NodeCanvas.Width / 2.)
-    Canvas.SetTop(node.Root, position.Y)  // - node.NodeCanvas.Height / 2.)
-
-    let drag_handler = initialize_rect_drag node window undo_record
     let color_handler = initialize_color_change node undo_record
     let delete_handler = initialize_deletion node window undo_record
 
-    node.Root :> UIElement
+    node
