4de9427d0ab6eb3150c6c01dd17c4aec31d7413f
Stephen Elliott <steell1890@gmail.com>
2014-04-03 15:08:02 -0400

    added some disposable helper methods

---
+++ ReactiveSandbox/ReactiveSandbox/DisposableExtensions.fs
@@ -0,0 +1,15 @@
+﻿module IDisposable
+
+open System
+
+let merge (d1 : IDisposable) (d2 : IDisposable) =
+    { new IDisposable with
+        member x.Dispose() =
+            d1.Dispose()
+            d2.Dispose() }
+
+let merge_all (ds : IDisposable seq) =
+    { new IDisposable with
+        member x.Dispose() =
+            for d in ds do
+                d.Dispose() }
